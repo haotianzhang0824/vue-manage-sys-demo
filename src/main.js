d99c0e0b6d1d4d9a15421a7d18de1b819896ea9e
tian <zht0824@gmail.com>
2023-05-08 08:28:33 +0800

    配置了axios全局配置

--- src/main.js
+++ src/main.js
@@ -1,22 +1,36 @@
+// 导入vue基本类
 import { createApp } from 'vue';
 import App from './App.vue';
-
+// 导入vue-router
 import router from './router';
+// 导入element plus和相关内容（来自于element-plus官网全局配置）
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// element plus图标库
+import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+// 导入axios
+import axios from 'axios';
 
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+// axios默认值配置。
+// 创建一个默认 Axios 实例
+const instance = axios.create({
+  baseURL: 'https://localhost:80/',
+  timeout: 5000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
 
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-
+// 配置app
 const app = createApp(App)
-
 app.use(router)
 app.use(ElementPlus)
-
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-    app.component(key, component)
-  }
-
+  app.component(key, component)
+}
+// 设置全局配置
+app.config.globalProperties.$axios = instance;
+// 挂载app
 app.mount('#app')
 
 
