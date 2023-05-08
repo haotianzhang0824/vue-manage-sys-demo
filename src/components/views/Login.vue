ae11690cde046c743d5823cb9e0ecc0a65f1b9e7
tian <zht0824@gmail.com>
2023-05-08 08:16:38 +0800

    略微修改了登录和注册页面

--- src/components/views/Login.vue
+++ src/components/views/Login.vue
@@ -1,23 +1,23 @@
 <template>
     <div style=" display: flex; justify-content: center; align-items: center;">
         <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm"
-            :size="formSize" status-icon>
-            <el-form-item label="username" prop="username">
-                <el-input v-model="ruleForm.username" style="width: 400px"/>
+            :size="formSize" status-icon labelPosition="right">
+            <el-form-item label="用户名" prop="username">
+                <el-input v-model="ruleForm.username" style="width: 400px" />
             </el-form-item>
-            <el-form-item label="password" prop="password">
+            <el-form-item label="密码" prop="password">
                 <el-input v-model="ruleForm.password" />
             </el-form-item>
-            <el-form-item label="phone" prop="phone">
+            <el-form-item label="手机号" prop="phone">
                 <el-input v-model="ruleForm.phone" />
             </el-form-item>
-            <el-form-item label="email" prop="email">
+            <el-form-item label="邮箱" prop="email">
                 <el-input v-model="ruleForm.email" />
             </el-form-item>
-            <el-form-item label="comment" prop="comment">
+            <el-form-item label="备注" prop="comment">
                 <el-input v-model="ruleForm.comment" />
             </el-form-item>
-            <el-form-item label="identity" prop="admin">
+            <el-form-item label="身份" prop="admin">
                 <el-select v-model="ruleForm.admin" placeholder="identity">
                     <el-option label="普通用户" value="0" />
                     <el-option label="管理员" value="1" />
@@ -25,9 +25,9 @@
             </el-form-item>
             <el-form-item>
                 <el-button type="primary" @click="submitForm(ruleFormRef)">
-                    Create
+                    登录
                 </el-button>
-                <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
+                <el-button @click="resetForm(ruleFormRef)">清空</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -51,13 +51,14 @@ const ruleForm = reactive({
 // 规则表
 const rules = reactive<FormRules>({
     username: [
-        { required: true, message: 'Please input username', trigger: 'blur' },
-        { min: 3, max: 8, message: 'Length should be 3 to 8', trigger: 'blur' },
-        {pattern: }
+        { required: true, message: '用户名由大写字母、小写字母、数字、下划线或连字符组成，要求3-18位', trigger: 'blur' },
+        { min: 3, max: 16, message: '请输入3-16位长度的用户名', trigger: 'blur' },
+        { pattern: "^[a-zA-Z0-9_-]{3,16}$", message: '用户名不符合规范' } // 这个表达式验证用户名必须由3到16个字符组成，可以是大写字母、小写字母、数字、下划线或连字符。
     ],
     password: [
         { required: true, message: 'Please input password', trigger: 'blur' },
         { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
+        { pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" }
     ],
     phone: [
         { required: true, message: 'Please input password', trigger: 'blur' },
@@ -72,11 +73,11 @@ const rules = reactive<FormRules>({
         { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
     ],
     admin: [
-      {
-        required: true,
-        message: 'Please select Activity zone',
-        trigger: 'change',
-      },
+        {
+            required: true,
+            message: 'Please select Activity zone',
+            trigger: 'change',
+        },
     ],
 })
 
--- src/components/views/Register.vue
+++ src/components/views/Register.vue
@@ -1,175 +1,104 @@
 <template>
-    <el-form
-      ref="ruleFormRef"
-      :model="ruleForm"
-      :rules="rules"
-      label-width="120px"
-      class="demo-ruleForm"
-      :size="formSize"
-      status-icon
-    >
-      <el-form-item label="Activity name" prop="name">
-        <el-input v-model="ruleForm.name" />
+  <div style=" display: flex; justify-content: center; align-items: center;">
+    <el-form ref="ruleFormRef" :model="ruleForm" :rules="rules" label-width="120px" class="demo-ruleForm" :size="formSize"
+      status-icon labelPosition="right">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="ruleForm.username" style="width: 400px" />
       </el-form-item>
-      <el-form-item label="Activity zone" prop="region">
-        <el-select v-model="ruleForm.region" placeholder="Activity zone">
-          <el-option label="Zone one" value="shanghai" />
-          <el-option label="Zone two" value="beijing" />
+      <el-form-item label="密码" prop="password">
+        <el-input v-model="ruleForm.password" />
+      </el-form-item>
+      <!-- <el-form-item label="手机号" prop="phone">
+        <el-input v-model="ruleForm.phone" />
+      </el-form-item>
+      <el-form-item label="邮箱" prop="email">
+        <el-input v-model="ruleForm.email" />
+      </el-form-item>
+      <el-form-item label="备注" prop="comment">
+        <el-input v-model="ruleForm.comment" />
+      </el-form-item>
+      <el-form-item label="身份" prop="admin">
+        <el-select v-model="ruleForm.admin" placeholder="identity">
+          <el-option label="普通用户" value="0" />
+          <el-option label="管理员" value="1" />
         </el-select>
-      </el-form-item>
-      <el-form-item label="Activity count" prop="count">
-        <el-select-v2
-          v-model="ruleForm.count"
-          placeholder="Activity count"
-          :options="options"
-        />
-      </el-form-item>
-      <el-form-item label="Activity time" required>
-        <el-col :span="11">
-          <el-form-item prop="date1">
-            <el-date-picker
-              v-model="ruleForm.date1"
-              type="date"
-              label="Pick a date"
-              placeholder="Pick a date"
-              style="width: 100%"
-            />
-          </el-form-item>
-        </el-col>
-        <el-col class="text-center" :span="2">
-          <span class="text-gray-500">-</span>
-        </el-col>
-        <el-col :span="11">
-          <el-form-item prop="date2">
-            <el-time-picker
-              v-model="ruleForm.date2"
-              label="Pick a time"
-              placeholder="Pick a time"
-              style="width: 100%"
-            />
-          </el-form-item>
-        </el-col>
-      </el-form-item>
-      <el-form-item label="Instant delivery" prop="delivery">
-        <el-switch v-model="ruleForm.delivery" />
-      </el-form-item>
-      <el-form-item label="Activity type" prop="type">
-        <el-checkbox-group v-model="ruleForm.type">
-          <el-checkbox label="Online activities" name="type" />
-          <el-checkbox label="Promotion activities" name="type" />
-          <el-checkbox label="Offline activities" name="type" />
-          <el-checkbox label="Simple brand exposure" name="type" />
-        </el-checkbox-group>
-      </el-form-item>
-      <el-form-item label="Resources" prop="resource">
-        <el-radio-group v-model="ruleForm.resource">
-          <el-radio label="Sponsorship" />
-          <el-radio label="Venue" />
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="Activity form" prop="desc">
-        <el-input v-model="ruleForm.desc" type="textarea" />
       </el-form-item>
       <el-form-item>
         <el-button type="primary" @click="submitForm(ruleFormRef)">
-          Create
+          注册
         </el-button>
-        <el-button @click="resetForm(ruleFormRef)">Reset</el-button>
-      </el-form-item>
+        <el-button @click="resetForm(ruleFormRef)">清空</el-button>
+      </el-form-item> -->
     </el-form>
-  </template>
-  
-  <script lang="ts" setup>
-  import { reactive, ref } from 'vue'
-  import type { FormInstance, FormRules } from 'element-plus'
-  
-  const formSize = ref('default')
-  const ruleFormRef = ref<FormInstance>()
-  const ruleForm = reactive({
-    name: 'Hello',
-    region: '',
-    count: '',
-    date1: '',
-    date2: '',
-    delivery: false,
-    type: [],
-    resource: '',
-    desc: '',
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+import type { FormInstance, FormRules } from 'element-plus'
+
+const formSize = ref('default')
+const ruleFormRef = ref<FormInstance>()
+const ruleForm = reactive({
+  username: '',
+  password: '',
+  phone: '',
+  email: '',
+  comment: '',
+  admin: ''
+})
+
+// 规则表
+const rules = reactive<FormRules>({
+  username: [
+    { required: true, message: '用户名由大写字母、小写字母、数字、下划线或连字符组成，要求3-18位', trigger: 'blur' },
+    { min: 3, max: 16, message: '请输入3-16位长度的用户名', trigger: 'blur' },
+    { pattern: "^[a-zA-Z0-9_-]{3,16}$", message: '用户名不符合规范' } // 这个表达式验证用户名必须由3到16个字符组成，可以是大写字母、小写字母、数字、下划线或连字符。
+  ],
+  password: [
+    { required: true, message: 'Please input password', trigger: 'blur' },
+    { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
+    { pattern: "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$" }
+  ],
+  phone: [
+    { required: true, message: 'Please input password', trigger: 'blur' },
+    { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
+  ],
+  email: [
+    { required: true, message: 'Please input password', trigger: 'blur' },
+    { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
+  ],
+  comment: [
+    { required: true, message: 'Please input password', trigger: 'blur' },
+    { min: 6, max: 18, message: 'Length should be 6 to 18', trigger: 'blur' },
+  ],
+  admin: [
+    {
+      required: true,
+      message: 'Please select Activity zone',
+      trigger: 'change',
+    },
+  ],
+})
+
+const submitForm = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      console.log('submit!')
+    } else {
+      console.log('error submit!', fields)
+    }
   })
-  
-  const rules = reactive<FormRules>({
-    name: [
-      { required: true, message: 'Please input Activity name', trigger: 'blur' },
-      { min: 3, max: 5, message: 'Length should be 3 to 5', trigger: 'blur' },
-    ],
-    region: [
-      {
-        required: true,
-        message: 'Please select Activity zone',
-        trigger: 'change',
-      },
-    ],
-    count: [
-      {
-        required: true,
-        message: 'Please select Activity count',
-        trigger: 'change',
-      },
-    ],
-    date1: [
-      {
-        type: 'date',
-        required: true,
-        message: 'Please pick a date',
-        trigger: 'change',
-      },
-    ],
-    date2: [
-      {
-        type: 'date',
-        required: true,
-        message: 'Please pick a time',
-        trigger: 'change',
-      },
-    ],
-    type: [
-      {
-        type: 'array',
-        required: true,
-        message: 'Please select at least one activity type',
-        trigger: 'change',
-      },
-    ],
-    resource: [
-      {
-        required: true,
-        message: 'Please select activity resource',
-        trigger: 'change',
-      },
-    ],
-    desc: [
-      { required: true, message: 'Please input activity form', trigger: 'blur' },
-    ],
-  })
-  
-  const submitForm = async (formEl: FormInstance | undefined) => {
-    if (!formEl) return
-    await formEl.validate((valid, fields) => {
-      if (valid) {
-        console.log('submit!')
-      } else {
-        console.log('error submit!', fields)
-      }
-    })
-  }
-  
-  const resetForm = (formEl: FormInstance | undefined) => {
-    if (!formEl) return
-    formEl.resetFields()
-  }
-  
-  const options = Array.from({ length: 10000 }).map((_, idx) => ({
-    value: `${idx + 1}`,
-    label: `${idx + 1}`,
-  }))
-  </script>
+}
+
+const resetForm = (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  formEl.resetFields()
+}
+
+const options = Array.from({ length: 10000 }).map((_, idx) => ({
+  value: `${idx + 1}`,
+  label: `${idx + 1}`,
+}))
+</script>
